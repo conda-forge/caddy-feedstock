c16415a1920f00cea7282f59c9d3dada2ef44109
Pavel Zwerschke <pavelzw@gmail.com>
2023-08-18 12:55:27 +0200

    Add osx-arm64

---
+++ recipe/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd src
+go-licenses save . --save_path ../library_licenses
+cd ../xcaddy
+(
+    unset GOOS
+    unset GOARCH
+    go build cmd/xcaddy/main.go -o xcaddy
+)
+
+./xcaddy build "v$PKG_VERSION" --output "$PREFIX/bin/caddy"
+# Clear out cache to avoid file not removable warnings
+chmod -R u+w "$(go env GOPATH)" && rm -r "$(go env GOPATH)"
